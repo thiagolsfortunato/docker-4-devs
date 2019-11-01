af996c88d860f99923844c62812f3aa0a1f56c6e
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2019-11-01 02:14:16 -0300

    :tada: finish challenge

--- python/app.py
+++ python/app.py
@@ -22,13 +22,20 @@ def index():
     if request.method == 'POST':
         message = request.form.get("message")
         app.logger.info(message)
-        save_message(message)
+        resp = save_message(message)
+        return resp
     return html
 
 def save_message(message):
     NODE_API = "http://" + node_host + ":" + node_port + "/save"
     resp = requests.post(url = NODE_API, data = {'message': message})
-    return resp
+    print(resp)
+    if (resp):
+        response = """
+        <h3> Mensagem Salva com sucesso! </h3>
+        <button onClick="window.location.href=window.location.href;">Reload</button>
+        """
+    return response
     
 
 if __name__ == "__main__":
